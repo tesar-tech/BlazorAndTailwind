25b2518b02c99ad34dd352d2a67f71d6350a5105
tesar-tech <jan@tesar.tech>
2023-12-25 13:27:21 +0100

    .NET8 update

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
-using BlazorAndTailwind;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorAndTailwind;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
